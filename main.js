d78809522e61bf8f896d14c71e9d3a584081ce17
seanwsutter <74794351+seanwsutter@users.noreply.github.com>
2023-08-08 08:47:32 -0500

    done

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let username = document.getElementById("username"),
   email = document.getElementById("email"),
   password = document.getElementById("password");
 
-create small arrow function to stop code repitition
+  create small arrow function to stop code repitition
 function named id with argument id 2x
 
 let id = (id) => {
@@ -23,9 +23,41 @@ let id = (id) => {
 Must write return word when using arrow function ()=>{}, Unless you write it on a single line
 */
 let id = (id) => document.getElementById(id);
+let classes = (classes) => document.getElementsByClassName(classes);
 
-  
+// issue - 3 div classes with same name 'error', use getElementsByClassName
+// statement will select all divs with class name 'error' and store them in array
+// errorMsg = ['error', 'error', 'error']; // ['0(username)', '1(email)', '2(password)'],  
+// example, select email, errorMsg[1].innerHTML = "wrong email"
+
 let username = id("username"), 
   email = id("email"),
-  password = id("password");
+  password = id("password"),
+  form = id("form"),
+  errorMsg = classes("error"),
+  successIcon = classes("success-icon"),
+  failureIcon = classes("failure-icon");
+  // added submit event listener to form (clicking submit will run function)
+  
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
+  engine(username, 0, "Username cannot be blank");
+  engine(email, 1, "Email cannot be blank");
+  engine(password, 2, "Password cannot be blank");
+});
+
+let engine = (id, serial, message) => {
+  if (id.value.trim() === "") { // trim() will remove extra spaces in text input fields 
+    errorMsg[serial].innerHTML = message;
+    failureIcon[serial].style.opacity = "1";
+    successIcon[serial].style.opacity = "0";
+  } else {
+    errorMsg[serial].innerHTML = "";
+    failureIcon[serial].style.opacity = "0";
+    successIcon[serial].style.opacity = "1";
+  }
+};
+
+
+  
